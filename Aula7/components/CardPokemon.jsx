71c2cce78fdf50e56859377b05728281d51ae8c1
AnnaLaura08 <annalaura.sales@icloud.com>
2026-03-19 14:02:25 -0300

    Atualização 3

--- Aula7/components/CardPokemon.jsx
+++ Aula7/components/CardPokemon.jsx
@@ -4,7 +4,7 @@ export function CardPokemon({ nome, foto, tipo, poder, numero }) {
   return (
     <div className={styles.card}>
       <img src={foto} alt={nome} className={styles.foto} />
-      <h3 className={styles.numero}>{numero}</h3>
+      <h3 className={styles.numero}><strong>Nº</strong>{numero}</h3>
       <h2 className={styles.nome}>{nome}</h2>
       <p className={styles.texto}>Tipo: {tipo}</p>
       <p className={styles.texto}>Poder: {poder}</p>
